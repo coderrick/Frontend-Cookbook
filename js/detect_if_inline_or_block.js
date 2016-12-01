e90473315cefd89be908db73ec2f745273d97947
coderrick <derrick.duncan@temple.edu>
2016-12-01 13:40:17 -0500

    Finished getViewPortSize function

--- js/detect_if_inline_or_block.js
+++ js/detect_if_inline_or_block.js
@@ -1,3 +1,7 @@
+/**
+ * Credit: http://stackoverflow.com/questions/2880957/detect-inline-block-type-of-a-dom-element
+ */
+
 function getDisplayType (element) {
     var cStyle = element.currentStyle || window.getComputedStyle(element, ""); 
     return cStyle.display;
--- js/util.js
+++ js/util.js
@@ -1,6 +1,12 @@
 /**
  * Look in to how to simulate private static access in JavaScript
+ * 
+ * Helpful sources:
+ * http://stackoverflow.com/questions/1248081/get-the-browser-viewport-dimensions-with-javascript
+ * 
  */
 function getViewportSize(){
-    
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+    console.log('viewport-width = '+ w + 'viewport-height = ' + h);
 }

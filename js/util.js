d5b3f689a945451f85df3488fd0b515d6266f14e
coderrick <derrick.duncan@temple.edu>
2016-12-01 23:57:05 -0500

    Update to util and canvas.html

--- js/util.js
+++ js/util.js
@@ -5,8 +5,9 @@
  * http://stackoverflow.com/questions/1248081/get-the-browser-viewport-dimensions-with-javascript
  * 
  */
-function getViewportSize(){
+function viewport_size(){
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
     var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
-    console.log('viewport-width = '+ w + 'viewport-height = ' + h);
+    console.log('viewport-width = '+ w + ', viewport-height = ' + h);
 }
+viewport_size();

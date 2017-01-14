695934a5547389ffe5d880bf68a7542c2d9a40c1
coderrick <derrick.duncan@temple.edu>
2017-01-13 21:53:03 -0500

    Made updates to monad.js

--- js/monad.js
+++ js/monad.js
@@ -1,4 +1,17 @@
 /**
  * Credit: https://curiosity-driven.org/monads-in-javascript
+ * Real-deal: http://mrbool.com/getting-started-with-java-monads/34302
  */
 "use strict";
+
+function multiplyBy2(num){
+  return num * 2;
+}
+
+function divideBy4(num){
+  return num/4;
+}
+
+function round(num){
+  return Math.round(num);
+}

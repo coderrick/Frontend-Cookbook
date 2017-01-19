2279ab316f907367487bc4ec5d40c21e8b28fa8b
coderrick <derrick.duncan@temple.edu>
2017-01-18 21:08:30 -0500

    updates

--- js/algorithms/reverse_string.js
+++ js/algorithms/reverse_string.js
@@ -3,6 +3,7 @@
  * Basically a rip off from "The C Programming Language"
  * Credit: http://www.c4learn.com/c-programs/c-program-reverse-string-without-using.html sort of
  * http://stackoverflow.com/questions/958908/how-do-you-reverse-a-string-in-place-in-javascript
+ * http://stackoverflow.com/questions/12132178/using-join-method-to-convert-array-to-string-without-commas
  */
 
 //Note Javascript strings are IMMUTABLE so the string must be converted to an array first
---
+++ js/algorithms/string_unique.js
@@ -0,0 +1,5 @@
+"use strict";
+/**
+ * http://stackoverflow.com/questions/1728per5948/javascript-determine-if-all-characters-in-a-string-are-unique-and-if-not-delet
+ * 
+ */

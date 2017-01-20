757191221351b26cec285beb50e6b453aae8d2c3
coderrick <derrick.duncan@temple.edu>
2017-01-19 23:47:42 -0500

    updates

---
+++ js/algorithms/matrix_zero.js
@@ -0,0 +1,13 @@
+"use strict";
+/**
+ * Credit: My Java NetBeans projects from school *Pats self on the back*
+ */
+
+function ma(){
+  m = [
+  [1,2,9],
+  [3,4,2],
+  [8,5,3]
+  ];
+console.log(m.join("\n"));
+}

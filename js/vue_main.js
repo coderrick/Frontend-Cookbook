29f55e54bc1fb10d4ba36d8b2f8ba6af49a1b705
coderrick <derrick.duncan@temple.edu>
2017-01-27 23:43:02 -0500

    vue update remeber to add html

--- js/vue_main.js
+++ js/vue_main.js
@@ -33,3 +33,16 @@ var app4 = new Vue({
     ]
   }
 })
+
+//User input
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split().reverse().join('')
+    }
+  }
+})

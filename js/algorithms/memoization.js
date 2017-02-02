ef8a365508a857364e97f0d9377c2f8723e6192f
coderrick <derrick.duncan@temple.edu>
2017-02-02 07:02:25 -0500

    Added first memoized function

---
+++ js/algorithms/memoization.js
@@ -0,0 +1,10 @@
+let powerHunna = function(base){
+  powerHunna._cache = powerHunna._cache || {};
+  if(!powerHunna._cache[base]){
+    console.log("Not in cache, computing...")
+    let result;
+    result = Math.pow(base, 100);
+    powerHunna._cache[base] = result;
+  }
+  return powerHunna._cache[base];
+}

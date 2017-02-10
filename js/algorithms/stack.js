f54129d3a177db4375098cd12f73f3e19c4d9ba4
coderrick <derrick.duncan@temple.edu>
2017-02-09 22:56:51 -0500

    updates

--- js/algorithms/stack.js
+++ js/algorithms/stack.js
@@ -1,4 +1,19 @@
 "use strict";
 /**
  * Credit:http://rebootjeff.github.io/blog/2014/01/04/queues-stacks-javascript-class-patterns/
+ * https://www.youtube.com/watch?v=sFVxsglODoo
+ * https://www.youtube.com/watch?v=oY4iNxteLJ4
  */
+let Stackk = function() {
+  this.top = -1;
+  this.a = [];
+}
+
+Stackk.prototype.push = function(i){
+  top += 1;
+  a[top] = i;
+}
+
+Stackk.prototype.pop = function(){
+  top -= top;
+}

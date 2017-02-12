a0f28e6bd7c414d57588c5264fa39fb626c90603
coderrick <derrick.duncan@temple.edu>
2017-02-11 23:58:58 -0500

    added another link to credits

--- js/algorithms/stack.js
+++ js/algorithms/stack.js
@@ -3,6 +3,7 @@
  * Credit:http://rebootjeff.github.io/blog/2014/01/04/queues-stacks-javascript-class-patterns/
  * https://www.youtube.com/watch?v=sFVxsglODoo
  * https://www.youtube.com/watch?v=oY4iNxteLJ4
+ * https://www.safaribooksonline.com/library/view/data-structures-and/9781449373931/ch04.html
  */
 let Stackk = function() {
   this.top = -1;

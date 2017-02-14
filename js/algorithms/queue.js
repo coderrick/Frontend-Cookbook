d9e222427cf2bf8a7726d67659efd355b40b1f18
coderrick <derrick.duncan@temple.edu>
2017-02-13 23:59:16 -0500

    Implemented boiler for queue

--- js/algorithms/queue.js
+++ js/algorithms/queue.js
@@ -3,3 +3,9 @@
  * Credit:http://rebootjeff.github.io/blog/2014/01/04/queues-stacks-javascript-class-patterns/
  * https://www.youtube.com/watch?v=okr-XE8yTO8
  */
+
+class Queue{
+  constructor(){
+    
+  }
+}

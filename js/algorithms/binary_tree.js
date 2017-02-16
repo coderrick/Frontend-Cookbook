d0911a5b0ef56396b3c96deacf6261e3863eae6b
coderrick <derrick.duncan@temple.edu>
2017-02-15 23:47:48 -0500

    Fighting with scope in binary_treejs

--- js/algorithms/binary_tree.js
+++ js/algorithms/binary_tree.js
@@ -3,3 +3,25 @@
  * Credit: https://www.nczonline.net/blog/2009/06/09/computer-science-in-javascript-binary-search-tree-part-1/
  * 
  */
+
+class Tree {
+  constructor(data){
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  } 
+}
+
+Tree.prototype.insertBST = function(root, data) {
+  if(root === null){
+    let root = new Tree(4);
+  }else if(data <= root.data){
+    root.left = insertBST(root.left, data);
+  }else{
+    root.right = insertBST(root.right, data);
+  }
+  return root;
+}
+
+let t = new Tree(1);
+t.insertBST(t, 5);

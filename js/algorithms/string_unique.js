1faa0e4d5f98c0ce399280251851b33078d74da4
coderrick <derrick.duncan@temple.edu>
2017-02-18 03:12:39 -0500

    Added string_unique solution

--- js/algorithms/string_unique.js
+++ js/algorithms/string_unique.js
@@ -1,5 +1,17 @@
 "use strict";
 /**
  * http://stackoverflow.com/questions/1728per5948/javascript-determine-if-all-characters-in-a-string-are-unique-and-if-not-delet
+ * http://stackoverflow.com/questions/19484406/detecting-if-a-string-has-unique-characters-comparing-my-solution-to-cracking
  * 
  */
+
+function unq(str){
+  let unique = new Map();
+  for(i = 0; i < str.length; i++){
+    unique.set(str.charAt(i));
+  }
+  if(unique.size!= str.length){
+    return false;
+  }
+  return true;
+}

b36e0504958c622a516bcc9b46b75516963bd095
coderrick <derrick.duncan@temple.edu>
2017-02-24 23:39:51 -0500

    Created graph object

--- js/algorithms/Graph.js
+++ js/algorithms/Graph.js
@@ -2,3 +2,10 @@
  * Djikstra's Algorithm yada yada...
  */
 "use strict";
+
+class Graph{
+  constructor(vertice_arr, edge_arr){
+    this.vertice_arr = vertice_arr;
+    this.edge_arr = edge_arr;
+  }
+}
